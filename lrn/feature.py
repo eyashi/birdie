97e64f113d88221c17defe9b1b4d939619391a05
eyashi <eyashin@umich.edu>
2019-07-15 22:19:58 -0700

    made some things my own

--- lrn/feature.py
+++ lrn/feature.py
@@ -1,8 +1,5 @@
-import wave
 import numpy as np
-import utils
 import librosa
-from IPython import embed
 import os
 from sklearn import preprocessing
 
@@ -36,56 +33,7 @@ def load_audio(filename, mono=True, fs=22050):
     """
 
     file_base, file_extension = os.path.splitext(filename)
-    if file_extension == '.wav':
-        _audio_file = wave.open(filename)
-
-        # Audio info
-        sample_rate = _audio_file.getframerate()
-        sample_width = _audio_file.getsampwidth()
-        number_of_channels = _audio_file.getnchannels()
-        number_of_frames = _audio_file.getnframes()
-
-        # Read raw bytes
-        data = _audio_file.readframes(number_of_frames)
-        _audio_file.close()
-
-        # Convert bytes based on sample_width
-        num_samples, remainder = divmod(len(data), sample_width * number_of_channels)
-        if remainder > 0:
-            raise ValueError('The length of data is not a multiple of sample size * number of channels.')
-        if sample_width > 4:
-            raise ValueError('Sample size cannot be bigger than 4 bytes.')
-
-        if sample_width == 3:
-            # 24 bit audio
-            a = np.empty((num_samples, number_of_channels, 4), dtype=np.uint8)
-            raw_bytes = np.fromstring(data, dtype=np.uint8)
-            a[:, :, :sample_width] = raw_bytes.reshape(-1, number_of_channels, sample_width)
-            a[:, :, sample_width:] = (a[:, :, sample_width - 1:sample_width] >> 7) * 255
-            audio_data = a.view('<i4').reshape(a.shape[:-1]).T
-        else:
-            # 8 bit samples are stored as unsigned ints; others as signed ints.
-            dt_char = 'u' if sample_width == 1 else 'i'
-            a = np.fromstring(data, dtype='<%s%d' % (dt_char, sample_width))
-            audio_data = a.reshape(-1, number_of_channels).T
-
-        if mono:
-            # Down-mix audio
-            audio_data = np.mean(audio_data, axis=0)
-
-        # Convert int values into float
-        audio_data = audio_data / float(2 ** (sample_width * 8 - 1) + 1)
-
-        # Resample
-        if fs != sample_rate:
-            audio_data = librosa.core.resample(audio_data, sample_rate, fs)
-            sample_rate = fs
-
-        return audio_data, sample_rate
-
-    elif file_extension == '.mp3':
-        # requires ffmpeg
-        # also the above could probably be shortened as well...
+    if file_extension == '.wav' or file_extension == '.mp3':
         return librosa.load(filename)
 
     elif file_extension == '.npy':
@@ -117,31 +65,21 @@ def extract_mbe(_y, _sr, _nfft, _nb_mel):
 #              Main script starts here
 # ###################################################################
 
-is_mono = False
-__class_labels = {
-    'brakes squeaking': 0,
-    'car': 1,
-    'children': 2,
-    'large vehicle': 3,
-    'people speaking': 4,
-    'people walking': 5
-}
+is_mono = True
 
 # location of data.
-folds_list = [1, 2, 3, 4]
-evaluation_setup_folder = '/home/adavanne/taitoSharedData/DCASE2017/task_3/TUT-sound-events-2017-development/evaluation_setup'
-audio_folder = '/home/adavanne/taitoSharedData/DCASE2017/task_3/TUT-sound-events-2017-development/audio/street'
+evaluation_setup_folder = 'eval'
+audio_folder = 'samples'
 
 # Output
-feat_folder = '/home/adavanne/taitoSharedData/DCASE2017/task_3/feat/'
-utils.create_folder(feat_folder)
+feat_folder = 'feat'
 
 # User set parameters
 nfft = 2048
 win_len = nfft
 hop_len = win_len / 2
 nb_mel_bands = 40
-sr = 44100
+sr = 22050
 
 # -----------------------------------------------------------------------
 # Feature extraction and label generation
@@ -183,6 +121,7 @@ for audio_filename in os.listdir(audio_folder):
 # Feature Normalization
 # -----------------------------------------------------------------------
 
+# use os.listdir instead
 for fold in folds_list:
     train_file = os.path.join(evaluation_setup_folder, 'street_fold{}_train.txt'.format(1))
     evaluate_file = os.path.join(evaluation_setup_folder, 'street_fold{}_evaluate.txt'.format(1))
